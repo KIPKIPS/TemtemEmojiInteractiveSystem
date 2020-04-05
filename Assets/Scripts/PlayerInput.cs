437482145273425ce87c85e5d6766cb4b9b86c27
RECNEPS-KIPS <2432719656@qq.com>
2020-04-06 02:03:07 +0800

    实现显示的渐显效果

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -20,19 +20,29 @@ public class PlayerInput : MonoBehaviour {
     private GameObject actEmoji;
     private Image actOutside;
     public Color oriColor;
+
+    public AudioSource[] audios;
+    public AudioClip[] clips;
+    public int actIndex;
+    //public int last=-1;
     // Start is called before the first frame update
     void Start() {
         width = Screen.width;
         height = Screen.height;
         oriColor = outside[0].color;
+        audios = Camera.main.GetComponents<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetMouseButtonDown(0)&&audios[0].isPlaying==false) {
+            audios[0].clip=clips[4];
+            audios[0].Play();
+        }
         mousePos = Input.mousePosition;
         x = mousePos.x;
         y = mousePos.y;
-        Debug.Log(x + " " + y);
+        //Debug.Log(x + " " + y);
         if (Input.GetMouseButton(0)) {
             roulette.SetActive(true);
             if (x-width/2 < 0 && y-height/2 > 0) {
@@ -52,29 +62,43 @@ public class PlayerInput : MonoBehaviour {
             actEmoji.SetActive(true);
         }
         if (Input.GetMouseButtonUp(0)) {
+            if (audios[0].isPlaying==false) {
+                audios[0].clip = clips[5];
+                audios[0].Play();
+            }
             actInside.SetActive(false);
             inactInside.SetActive(true);
             roulette.SetActive(false);
             actOutside.color = oriColor;
             actEmoji.SetActive(false);
+            MouseUpEvent(actIndex);
             //TODO:设置动画表情
         }
-        
     }
 
+    void MouseUpEvent(int index) {
+        if (audios[1].isPlaying==false) {
+            audios[1].clip = clips[index];
+            audios[1].Play();
+        }
+    }
     void Change(int index) {
+        actIndex = index;
+        //if (last!=index) {
+            //Debug.Log(last+" "+index); 
+            //   audios[0].Play();
+        //}
         if (actInside != null && inactInside != null) {
             actInside.SetActive(false);
             inactInside.SetActive(true);
             actEmoji.SetActive(false);
             actInside.transform.parent.transform.parent.transform.Find("Outside").GetComponent<Image>().color = oriColor;
         }
-
         actEmoji=emoji[index];
         actInside = activeInsideList[index];
         inactInside = inactiveInsideList[index];
         actOutside = outside[index];
         outside[index].color = new Color(oriColor.r, oriColor.g, oriColor.b, oriColor.a + 0.17f);
-
+        //last = index;
     }
 }
